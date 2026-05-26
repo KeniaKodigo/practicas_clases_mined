1176c562290360f513f6fa5ac51adf37b8d00607
KeniaKodigo <kenia.paz@kodigo.org>
2026-05-25 20:30:16 -0600

    consultas sql parte 1

---
+++ consultasMinedSQL.sql
@@ -0,0 +1,10 @@
+-- los 2 guiones indican comentario
+-- Practica de consultas basicas SQL (DML)
+
+-- mostrar todos los alojamientos de la bd
+-- SELECT * FROM accomodations
+SELECT id, name, address, description, image FROM  accomodations
+
+-- insertar un nuevo alojamiento
+-- INSERT INTO accomodations (name, address, description, image) VALUES('Hostal Los Chacones', 'El Pital, San Ignacio', 'Hostal con una vista hacia al bosque', 'hola')
+
