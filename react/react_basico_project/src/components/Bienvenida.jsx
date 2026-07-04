39a1ecf50f6cf631e786cb5db34020bf27a3f879
KeniaKodigo <kenia.paz@kodigo.org>
2026-07-03 20:40:13 -0600

    introduccion a react

---
+++ react/react_basico_project/src/components/Bienvenida.jsx
@@ -0,0 +1,14 @@
+
+function Bienvenida() {
+    
+    return (
+        <div>
+            <h2>Bienvenido a mi lista de compras 🛒</h2>
+            <p>Un gusto de saludarte!</p>
+        </div>
+    )
+}
+
+export default Bienvenida
+
+
---
+++ react/react_basico_project/src/components/ListaCompras.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+export default function ListaCompras() {
+    return (
+        <div>
+            <ul>
+                <li>🍪 galletas</li>
+                <li>☕ cafe</li>
+                <li>🍇 uvas</li>
+            </ul>
+        </div>
+    )
+}
