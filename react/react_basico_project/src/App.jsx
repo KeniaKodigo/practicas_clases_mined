31175455f01c0800d284265d766306ae0355fc20
KeniaKodigo <kenia.paz@kodigo.org>
2026-07-06 21:06:22 -0600

    uso de props y estados

--- react/react_basico_project/src/App.jsx
+++ react/react_basico_project/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css'
 import Bienvenida from './components/Bienvenida'
 import ListaCompras from './components/ListaCompras'
@@ -5,6 +6,13 @@ import confetti from "@hiseb/confetti";
 
 // componente funcionales
 function App() {
+  // como declarar un estado
+  /**
+   * 2 valores a tomar en cuenta
+   * [0] = es el que se encarga de mostrar/guardar el estado
+   * [1] = es el que se encarga de actualizar el estado
+   */
+  const [contador, setContador] = useState(0) //valor inicial
 
   // funcion para tirar confeti
   const tirarConfetti = () => {
@@ -13,9 +21,11 @@ function App() {
 
   return (
     <>
-      <h1>Mi primer proyecto de react</h1>
+      <h1 className="titulo">Mi primer proyecto de react</h1>
       {/** llamando nuestros componentes y los podemos reutilizar */}
-      <Bienvenida></Bienvenida>
+
+      {/** pasando datos como props */}
+      <Bienvenida usuario="Martha Santos" confetti={tirarConfetti} ></Bienvenida>
       <ListaCompras />
       <ListaCompras />
       <ListaCompras />
--- react/react_basico_project/src/components/Bienvenida.jsx
+++ react/react_basico_project/src/components/Bienvenida.jsx
@@ -1,10 +1,17 @@
 
-function Bienvenida() {
-    
+// recibiendo datos de otro componente
+// desestructuracion de elementos (props)
+function Bienvenida({usuario, confetti}) {
+    // parte funcional/js
+    let mensaje = "Hola 👋";
+
+    // lo que va ver el usuario
     return (
         <div>
             <h2>Bienvenido a mi lista de compras 🛒</h2>
-            <p>Un gusto de saludarte!</p>
+            <p>{mensaje} Un gusto de saludarte {usuario}!</p>
+
+            <button onClick={confetti}>Bienvenido 🎊</button>
         </div>
     )
 }
